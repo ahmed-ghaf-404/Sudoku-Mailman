152cae7dd971d9ed32dbd6d41587fbb9c09e0e5f
Ahmed Alghafri <50543745+ahmed-ghaf-404@users.noreply.github.com>
2023-05-07 01:19:22 -0400

    minor changes

--- sudoku_mailman/engine.py
+++ sudoku_mailman/engine.py
@@ -3,5 +3,5 @@ from sqlalchemy.orm import sessionmaker
 
 CONNSTR = 'postgresql://sudoku.mailman:bOBUeN7adk1Q@ep-patient-math-334326.us-east-2.aws.neon.tech/users'
 
-Engine = create_engine(CONNSTR , connect_args={'options': '-csearch_path=users'}, sslmode='require')
+Engine = create_engine(CONNSTR , connect_args={'options': '-csearch_path=users'}, drivername="postgresql+psycopg2-binary", sslmode='require')
 Session = sessionmaker(bind=Engine)
