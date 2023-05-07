ddb0ca5c00a6656e40da7db56b0ded3af2ae81b8
Ahmed Alghafri <50543745+ahmed-ghaf-404@users.noreply.github.com>
2023-05-07 01:16:42 -0400

    added sslmode

--- sudoku_mailman/engine.py
+++ sudoku_mailman/engine.py
@@ -3,5 +3,5 @@ from sqlalchemy.orm import sessionmaker
 
 CONNSTR = 'postgresql://sudoku.mailman:bOBUeN7adk1Q@ep-patient-math-334326.us-east-2.aws.neon.tech/users'
 
-Engine = create_engine(CONNSTR , connect_args={'options': '-csearch_path=users'})
+Engine = create_engine(CONNSTR , connect_args={'options': '-csearch_path=users'}, sslmode='require')
 Session = sessionmaker(bind=Engine)
