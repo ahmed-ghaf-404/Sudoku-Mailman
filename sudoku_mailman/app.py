5225e52f3db7387cb8057dce913e129f41764bfd
Ahmed Alghafri <50543745+ahmed-ghaf-404@users.noreply.github.com>
2023-05-07 01:31:42 -0400

    this aint gonna work

--- sudoku_mailman/app.py
+++ sudoku_mailman/app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, request
-from sudoku_mailman.engine import Engine
+from sudoku_mailman.engine import Session
 from sudoku_mailman.Subscriber import Subscriber
 # from flask_mail import Mail, Message
 from sudoku_mailman.send_sudoku import send_sudoku
 from apscheduler.schedulers.background import BackgroundScheduler
-from sqlalchemy.orm import sessionmaker
 import requests
 
 app = Flask(__name__)
@@ -17,7 +16,7 @@ def index():
 @app.route('/subscribe', methods=['POST'])
 def subscribe():
     email = request.form['email']
-    with sessionmaker(Engine).Session() as session:
+    with Session() as session:
         if session.query(Subscriber).filter_by(email=email).first():
             return render_template('index.html')
         new_subscriber = Subscriber(email=email)
@@ -27,7 +26,7 @@ def subscribe():
 @app.route('/unsubscribe', methods=['POST'])
 def unsubscribe():
     email = request.form['email']
-    with sessionmaker(Engine).Session() as session:
+    with Session() as session:
         subscriber = session.query(Subscriber).filter_by(email=email).first()
         # if the subscriber exists, delete it from the database
         if subscriber:
@@ -37,17 +36,17 @@ def unsubscribe():
 
 @app.route('/send-email', methods=['POST'])
 def send_email_to_subscribers():
-    with sessionmaker(Engine).Session() as session:
+    with Session() as session:
         subscribers = session.query(Subscriber).all()
         for subscriber in subscribers:
             send_sudoku(recipient_email=subscriber.email)
     return "index.html"
 
-def send_emails_job():
-    url = 'http://127.0.0.1:5000/send-email'
-    response = requests.post(url)
-    print(response.text)
-    # send_email_to_subscribers()
+# def send_emails_job():
+#     url = 'http://127.0.0.1:5000/send-email'
+#     response = requests.post(url)
+#     print(response.text)
+#     # send_email_to_subscribers()
 
 # scheduler = BackgroundScheduler()
 # scheduler.add_job(send_emails_job, 'cron', day_of_week='mon-sun', hour=2, minute=51)
--- sudoku_mailman/engine.py
+++ sudoku_mailman/engine.py
@@ -4,3 +4,4 @@ from sqlalchemy.orm import sessionmaker
 CONNSTR = 'postgresql://sudoku.mailman:bOBUeN7adk1Q@ep-patient-math-334326.us-east-2.aws.neon.tech/users'
 
 Engine = create_engine(CONNSTR , connect_args={'options': '-csearch_path=users'})
+Session = sessionmaker(bind=Engine)
