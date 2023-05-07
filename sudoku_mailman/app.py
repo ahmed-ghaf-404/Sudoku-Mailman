58a15f42d772687e0e3ba62644a4cef54c1b9e95
Ahmed Alghafri <50543745+ahmed-ghaf-404@users.noreply.github.com>
2023-05-07 01:23:59 -0400

    -

--- sudoku_mailman/app.py
+++ sudoku_mailman/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request
-from sudoku_mailman.engine import Session
+from sudoku_mailman.engine import Engine
 from sudoku_mailman.Subscriber import Subscriber
 # from flask_mail import Mail, Message
 from sudoku_mailman.send_sudoku import send_sudoku
 from apscheduler.schedulers.background import BackgroundScheduler
+from sqlalchemy.orm import sessionmaker
 import requests
 
 app = Flask(__name__)
@@ -16,7 +17,7 @@ def index():
 @app.route('/subscribe', methods=['POST'])
 def subscribe():
     email = request.form['email']
-    with Session() as session:
+    with sessionmaker(Engine).Session() as session:
         if session.query(Subscriber).filter_by(email=email).first():
             return render_template('index.html')
         new_subscriber = Subscriber(email=email)
@@ -26,7 +27,7 @@ def subscribe():
 @app.route('/unsubscribe', methods=['POST'])
 def unsubscribe():
     email = request.form['email']
-    with Session() as session:
+    with sessionmaker(Engine).Session() as session:
         subscriber = session.query(Subscriber).filter_by(email=email).first()
         # if the subscriber exists, delete it from the database
         if subscriber:
@@ -36,7 +37,7 @@ def unsubscribe():
 
 @app.route('/send-email', methods=['POST'])
 def send_email_to_subscribers():
-    with Session() as session:
+    with sessionmaker(Engine).Session() as session:
         subscribers = session.query(Subscriber).all()
         for subscriber in subscribers:
             send_sudoku(recipient_email=subscriber.email)
@@ -48,9 +49,9 @@ def send_emails_job():
     print(response.text)
     # send_email_to_subscribers()
 
-scheduler = BackgroundScheduler()
-scheduler.add_job(send_emails_job, 'cron', day_of_week='mon-sun', hour=2, minute=51)
-scheduler.start()
+# scheduler = BackgroundScheduler()
+# scheduler.add_job(send_emails_job, 'cron', day_of_week='mon-sun', hour=2, minute=51)
+# scheduler.start()
 
 if __name__ == '__main__':
     app.run(debug=False)
