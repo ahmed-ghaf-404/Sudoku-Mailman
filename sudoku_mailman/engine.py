27d2f504740cd2eb2c610b6a84974f321e553ef2
Ahmed Alghafri <50543745+ahmed-ghaf-404@users.noreply.github.com>
2023-05-07 01:42:37 -0400

    chatgpt oclock

--- sudoku_mailman/engine.py
+++ sudoku_mailman/engine.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-CONNSTR = 'postgresql://sudoku.mailman:bOBUeN7adk1Q@ep-patient-math-334326.us-east-2.aws.neon.tech/users'
+CONNSTR = 'postgresql://sudoku.mailman:bOBUeN7adk1Q@ep-patient-math-334326.us-east-2.aws.neon.tech/users?sslmode=require'
 
 Engine = create_engine(CONNSTR , connect_args={'options': '-csearch_path=users'})
 Session = sessionmaker(bind=Engine)
