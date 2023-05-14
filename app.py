22722e2f8e14efd757c14bb91152291f220b1128
Ahmed Alghafri <50543745+ahmed-ghaf-404@users.noreply.github.com>
2023-05-14 12:38:13 -0400

    app -> global

--- app.py
+++ app.py
@@ -5,15 +5,16 @@ from sudoku_mailman.routes.unsubscribe import unsubscribe_bp
 from sudoku_mailman.routes.send_sample import send_sample_bp
 from sudoku_mailman.routes.notify_all_subscribers import notify_all_subscribers_bp 
 
+app = Flask(__name__, template_folder='sudoku_mailman/templates', static_folder='sudoku_mailman/static')
+app.run(debug=False)
 
-def main():
-    app = Flask(__name__, template_folder='sudoku_mailman/templates', static_folder='sudoku_mailman/static')
+def main():    
     app.register_blueprint(index_bp)
     app.register_blueprint(subscribe_bp)
     app.register_blueprint(unsubscribe_bp)
     app.register_blueprint(send_sample_bp)
     app.register_blueprint(notify_all_subscribers_bp)
-    app.run(debug=False)
+    
 
 if __name__ == '__main__':
     main()
