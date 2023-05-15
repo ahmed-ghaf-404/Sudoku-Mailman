73eeca315061532f008e61b93894e8e00e633a2c
Ahmed Alghafri <50543745+ahmed-ghaf-404@users.noreply.github.com>
2023-05-14 20:05:41 -0400

    start of testlayout

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask
 from sudoku_mailman.routes.index import index_bp
-from sudoku_mailman.routes.subscribe import subscribe_bp
-from sudoku_mailman.routes.unsubscribe import unsubscribe_bp
-from sudoku_mailman.routes.send_sample import send_sample_bp
-from sudoku_mailman.routes.notify_all_subscribers import notify_all_subscribers_bp 
+# from sudoku_mailman.routes.subscribe import subscribe_bp
+# from sudoku_mailman.routes.unsubscribe import unsubscribe_bp
+# from sudoku_mailman.routes.send_sample import send_sample_bp
+# from sudoku_mailman.routes.notify_all_subscribers import notify_all_subscribers_bp 
 from sudoku_mailman.base import PORT,HOST
 
 
 app = Flask(__name__, template_folder='sudoku_mailman/templates', static_folder='sudoku_mailman/static')
 app.register_blueprint(index_bp)
-app.register_blueprint(subscribe_bp)
-app.register_blueprint(unsubscribe_bp)
-app.register_blueprint(send_sample_bp)
-app.register_blueprint(notify_all_subscribers_bp)
+# app.register_blueprint(subscribe_bp)
+# app.register_blueprint(unsubscribe_bp)
+# app.register_blueprint(send_sample_bp)
+# app.register_blueprint(notify_all_subscribers_bp)
 app.run(debug=True, port=PORT, host=HOST)
 
 
